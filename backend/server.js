36af2dfa7d082da0e87050c7b494b941cd65319b
Eng.Mahmoud_reda <mahmoudreda6363@gmail.com>
2026-02-07 22:51:16 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const app = require('./app');
-const connectDB = require('./config/db'); // <-- 1. استيراد دالة الاتصال
+const connectDB = require('./config/db');
 require('dotenv').config();
 
-// Connect to Database
-connectDB(); // <-- 2. تشغيل دالة الاتصال
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}` );
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}` );
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+  }
+};
 
+startServer();
